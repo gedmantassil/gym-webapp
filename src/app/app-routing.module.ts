87366ce26eb077dbaf681f68bbf75d2e754df0f5
gedmantassil <160595757+gedmantassil@users.noreply.github.com>
2024-03-05 23:57:56 +0200

    RTE1 INIT
    Fixed rooting to the main page of the website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { CoursesComponent } from './courses/courses.component';
 import { ContactsComponent } from './contacts/contacts.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   {path: 'selection-core', component: SelectionCoreComponent},
   {path: 'courses', component: CoursesComponent},
